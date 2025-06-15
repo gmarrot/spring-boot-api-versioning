541e488cbbae8a3c33bf590421c7f37f8500cd00
Gaëtan Marrot <gaetan.marrot@gmail.com>
2025-06-15 20:32:09 +0200

    Fix path versioning mode when method and class are both annotated

--- library/src/main/kotlin/com/betomorrow/spring/mvc/versions/ApiVersionPathRequestMappingHandlerMapping.kt
+++ library/src/main/kotlin/com/betomorrow/spring/mvc/versions/ApiVersionPathRequestMappingHandlerMapping.kt
@@ -1,6 +1,7 @@
 package com.betomorrow.spring.mvc.versions
 
 import org.springframework.core.annotation.AnnotatedElementUtils
+import org.springframework.core.annotation.AnnotationUtils
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping
@@ -12,8 +13,12 @@ class ApiVersionPathRequestMappingHandlerMapping(
     private val pathVersionPrefix: String,
 ) : RequestMappingHandlerMapping() {
     override fun getMappingForMethod(method: Method, handlerType: Class<*>): RequestMappingInfo? {
-        val methodRequestMappingInfo = this.createRequestMappingInfo(method) ?: return null
-        val typeRequestMappingInfo = this.createRequestMappingInfo(handlerType)
+        val allowedApiVersions =
+            getAllowedApiVersions(method, handlerType)
+                ?: return super.getMappingForMethod(method, handlerType)
+
+        val methodRequestMappingInfo = this.createRequestMappingInfo(method, allowedApiVersions) ?: return null
+        val typeRequestMappingInfo = this.createRequestMappingInfo(handlerType, allowedApiVersions)
         return if (typeRequestMappingInfo != null) {
             typeRequestMappingInfo.combine(methodRequestMappingInfo)
         } else {
@@ -21,18 +26,20 @@ class ApiVersionPathRequestMappingHandlerMapping(
         }
     }
 
-    private fun createRequestMappingInfo(element: AnnotatedElement): RequestMappingInfo? {
+    private fun getAllowedApiVersions(method: Method, handlerType: Class<*>): Set<ApiVersion>? {
+        val requestApiVersion =
+            AnnotationUtils.findAnnotation(method, RequestApiVersion::class.java)
+                ?: AnnotationUtils.findAnnotation(handlerType, RequestApiVersion::class.java)
+                ?: return null
+        return requestApiVersion.value.map { ApiVersion(it) }.toSet()
+    }
+
+    private fun createRequestMappingInfo(element: AnnotatedElement, apiVersions: Set<ApiVersion>): RequestMappingInfo? {
         val originalMapping = AnnotatedElementUtils.findMergedAnnotation(element, RequestMapping::class.java) ?: return null
-        val requestApiVersion = AnnotatedElementUtils.findMergedAnnotation(element, RequestApiVersion::class.java)
 
-        if (requestApiVersion == null) {
-            return this.createRequestMappingInfo(originalMapping, null)
-        }
-
-        val allowedApiVersions = requestApiVersion.value.map { ApiVersion(it) }.toSet()
         val paths =
             originalMapping.path.flatMap { originalPath ->
-                allowedApiVersions.map { version -> originalPath.replace(pathVariableName, "$pathVersionPrefix${version.value}") }
+                apiVersions.map { version -> originalPath.replace(pathVariableName, "$pathVersionPrefix${version.value}") }
             }
 
         return RequestMappingInfo
@@ -42,6 +49,7 @@ class ApiVersionPathRequestMappingHandlerMapping(
             .headers(*originalMapping.headers)
             .consumes(*originalMapping.consumes)
             .produces(*originalMapping.produces)
+            .mappingName(originalMapping.name)
             .build()
     }
 }
